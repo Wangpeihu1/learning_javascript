b3dd0e46637a4ca213147c1a4560effaea7de6be
Wangpeihu1 <w18817392281@163.com>
2018-06-16 11:42:56 +0800

    web练习
    JavaScript练习

--- htmlcss_test/test_site/scripts/main.js
+++ htmlcss_test/test_site/scripts/main.js
@@ -1,6 +1,32 @@
 /*
 Everythings in between is a comment.
-*/
-// This is a comment.
+
 var myHeading = document.getElementsByTagName('h1')[0];
 myHeading.innerHTML = 'Hello World!'
+*/
+var myImage = document.getElementsByTagName('img')[0];
+
+myImage.onclick = function(){
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/123.jpg'){
+		myImage.setAttribute('src','images/456.jpg');
+	} else {
+		myImage.setAttribute('src','images/123.jpg');
+	}
+}
+var myButton = document.getElementsByTagName('button')[0];
+var myHeading = document.getElementsByTagName('h1')[0];
+function setUserName(){
+	var myName = prompt('Please enter your name');
+	localStorage.setItem('name',myName);
+	myHeading.innerHTML = 'Mozilla is cool,'+myName;
+}
+if(!localStorage.getItem('name')){
+	setUserName();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.innerHTML = 'Mozilla is cool,'+storedName;
+}
+myButton.onclick = function(){
+	setUserName();
+}
